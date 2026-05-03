fa8c7877f4e1b4761aa8ee078b759ee834f85bb4
rei1237 <rei1237@users.noreply.github.com>
2026-05-03 16:13:10 +0900

    fix: 속마음 알아보기 타로를 MindScanTarot 컴포넌트를 직접 실행하도록 수정

--- app/tarot/mindscan/page.tsx
+++ app/tarot/mindscan/page.tsx
@@ -1,30 +1,7 @@
-import FeatureLandingPage from "../../components/FeatureLandingPage";
+"use client";
 
-export const metadata = {
-  title: "상대방의 정확한 속마음 알아보기 타로 | Code Destiny",
-  description:
-    "10카드 십자 스프레드로 상대방의 진짜 속마음·감정·욕구를 타로 카드로 읽어드립니다. 글래스모피즘 Midnight Mystery 디자인의 몰입형 타로 리딩.",
-  openGraph: {
-    title: "상대방의 속마음 알아보기 타로 | Code Destiny",
-    description: "상대방의 진심, 숨겨진 감정, 바라는 미래를 10장 타로로 분석합니다.",
-    images: [{ url: "https://code-destiny.com/fuctionassets/mindscantaro.webp" }],
-  },
-};
+import MindScanTarot from "../../components/MindScanTarot";
 
 export default function MindScanTarotPage() {
-  return (
-    <FeatureLandingPage
-      service={{
-        h1: "🔮 속마음 알아보기 타로",
-        description: metadata.description,
-        ogImage: "https://code-destiny.com/fuctionassets/mindscantaro.webp",
-        landingPoints: [
-          "10카드 십자 스프레드",
-          "상대의 진짜 속마음 분석",
-          "관계 흐름/행동 조언",
-        ],
-        seoText: "실행은 /static 서비스 화면에서만 진행됩니다.",
-      }}
-    />
-  );
+  return <MindScanTarot />;
 }
