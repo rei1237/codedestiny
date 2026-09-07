9a016b6863e268400cc823abdd71a296d0afc4c7
네오 <seongbae555@gmail.com>
2026-09-08 02:32:07 +0900

    chore: refresh mirrors after main sync

--- js/core/bootstrapDestinyFlower.js
+++ js/core/bootstrapDestinyFlower.js
@@ -1,4 +1,4 @@
-import { registerFlowerArtGlobals } from '../services/destiny-flower-art.js?v=build-a8c0929fbd24';
+import { registerFlowerArtGlobals } from '../services/destiny-flower-art.js?v=build-44db5df346c0';
 
 /**
  * 🔴 매칭 엔진은 여기서 올리지 않는다. 2026-08-24 에 `worker/lib/destiny-flower-engine.js` 로
