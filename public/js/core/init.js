e5eb4bc10b7317ae2d7d1e179457826209c28e56
네오 <seongbae555@gmail.com>
2026-09-10 16:21:29 +0900

    chore(build): refresh public mirrors

--- public/js/core/init.js
+++ public/js/core/init.js
@@ -1,4 +1,4 @@
-import { bindGlobalActions } from './uiBindings.js?v=build-e88ef3e7a25b';
+import { bindGlobalActions } from './uiBindings.js?v=build-70e3a9af31d9';
 
 function applyProgressiveContainment(doc) {
   if (!doc || !doc.documentElement || !('contentVisibility' in doc.documentElement.style)) return;
